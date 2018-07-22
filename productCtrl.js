2be732730034a3954214579ae50248583b6595da
shmerling86 <shmerling86@gmail.com>
2018-07-22 07:25:31 +0300

    selected product

--- productCtrl.js
+++ productCtrl.js
@@ -3,36 +3,23 @@ app.controller('productCtrl', function ($scope, $log, productListSrv) {
     $scope.products = [];
 
     productListSrv.readFile().then(function (products) {
-        // //regular route
         $scope.products = products;
-
-
-        // //dynamic route
-        // $scope.products = products.filter(function (product) { if($routeParams.name == product.name) { return product } });
-
     }, function (error) {
         $log.error(error)
     });
 
     $scope.selected = [];
-    $scope.bool = false;
 
-    $scope.addToCart = function($index) {
-        console.log($index);
+    $scope.addToCart = function () {
 
-}
-    // productListSrv.addToCart($index).then(function (product) {
-    //     $scope.selected.push(product);
-
-    //     $scope.searchText = '';
-    //     $scope.movies = [];
-    // });
-    
-
-    // $scope.filterOptions = 
-    // [
-    //     {presentation: "orderByZone", field: "zone"}
-    // ];
+        for (var i = 0; i < $scope.products.length; i++) {
+            if ($scope.products[i].isActive){
+                console.log($scope.products[i].isActive);
+                
+                // $scope.selected.push($scope.products[i]) 
+        }
+    }
+    };
 
 
 });
--- productListSrv.js
+++ productListSrv.js
@@ -1,13 +1,14 @@
 app.service('productListSrv', function ($http, $q) {
 
 
-    function Product(productName, description, price, zone, brand, image) {
+    function Product(productName, description, price, zone, brand, image, isActive) {
         this.productName = productName;
         this.description = description;
         this.price = price;
         this.zone = zone;
         this.brand = brand;
         this.image = image;
+        this.isActive = isActive
     }
 
     var products = [];
@@ -18,7 +19,7 @@ app.service('productListSrv', function ($http, $q) {
     
         $http.get('products.json').then(function (response) {
             response.data.forEach(function (plainObj) {
-                var product = new Product(plainObj.productName, plainObj.description, plainObj.price, plainObj.zone, plainObj.brand, plainObj.image);
+                var product = new Product(plainObj.productName, plainObj.description, plainObj.price, plainObj.zone, plainObj.brand, plainObj.image, plainObj.isActive);
                 products.push(product);
             }, function (response) {
                 console.error(response);
