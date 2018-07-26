0f898a2148f6ef9c2939a8285fb01aa97bed9af4
shmerling86 <shmerling86@gmail.com>
2018-07-26 11:10:00 +0300

    fix url in the index

--- app/model/randomUrlSrv.js
+++ app/model/randomUrlSrv.js
@@ -1,17 +1,17 @@
 app.factory('randomUrlSrv', function () {
 
-    function makeid() {
-        var text = "";
-        var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
-      
-        for (var i = 0; i < 5; i++)
-          text += possible.charAt(Math.floor(Math.random() * possible.length));
-        return text;
-      }
-    
-    return {
-       makeid: makeid
-    }
+  function makeid() {
+    var text = "";
+    var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+
+    for (var i = 0; i < 5; i++)
+      text += possible.charAt(Math.floor(Math.random() * possible.length));
+    return text;
+  }
+
+  return {
+    makeid: makeid
+  }
 
 });
 
