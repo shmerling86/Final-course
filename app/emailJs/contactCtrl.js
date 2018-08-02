d4e908bb3beb7ae244288f50896e2d94ff81018e
shmerling86 <shmerling86@gmail.com>
2018-08-02 11:56:05 +0300

    contact complete+emailJS

--- app/emailJs/contactCtrl.js
+++ app/emailJs/contactCtrl.js
@@ -12,7 +12,13 @@ app.controller('contactCtrl', function($scope){
      var template_id = "template_dlc0Ty6L";
     $scope.sendEmail = function() {
         emailjs.send(service_id,template_id,$scope.template_params);
-        alert("mail sent");
+        alert("Mail sent!");
+        $scope.template_params = {
+            reply_to: "",
+            from_name: "",
+            from_email: "",
+            message_html: ""
+         }
     }
 
 
