5b625de290e327fd8a83fc7c2c123acdde7289de
shmerling86 <shmerling86@gmail.com>
2018-08-09 10:02:49 +0300

    fix bug

--- app/app.js
+++ app/app.js
@@ -32,7 +32,7 @@ app.config(function ($routeProvider) {
 
         })
         .when("/codeApprove", {
-            templateUrl: "app/myLink/codeApprove.html",
+            templateUrl: "app/myLink/codeApprove.html"
 
         })
         .when("/guestLogin", {
